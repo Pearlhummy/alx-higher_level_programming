3e14d02745cd05dec725893df52c90461a1231ea
Pearlhummy <igbinladepelumi@gmail.com>
2022-10-26 01:50:03 +0000

    task 1 updated

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -1,41 +1,61 @@
 #!/usr/bin/python3
 """
-This is a Rectangle class.
+This module defines a class - Rectangle
 """
 
 
 class Rectangle:
     """
-    Initialize Rectangle object with height and width.
+    This class has two attributes
+    width
+    height
+    both will have property and setter function definition
     """
 
     def __init__(self, width=0, height=0):
-        self.__width = width
-        self.__height = height
+        """
+        instantiates width and height
+        """
+        self.width = width
+        self.height = height
 
     @property
     def width(self):
-        """get rectangle width
+        """
+        function to return width if setter checks have passed
         """
         return self.__width
 
-    @property
-    def height(self):
-        """rectangle height
-        """
-        return self.__height
-
     @width.setter
     def width(self, value):
-        if type(value) is not int:
+        """
+        setter validates if value is >= 0
+        Raises:
+        TypeError
+        ValueError
+        """
+        if not isinstance(value, int):
             raise TypeError("width must be an integer")
-        if value <= 0:
+        if value < 0:
             raise ValueError("width must be >= 0")
         self.__width = value
 
+    @property
+    def height(self):
+        """
+        function to return height if setter checks have passed
+        """
+        return self.__height
+
     @height.setter
     def height(self, value):
-        if type(value) is not int:
+        """
+        setter validates if value is >= 0
+        Raises:
+        TypeError
+        ValueError
+        """
+        if not isinstance(value, int):
             raise TypeError("height must be an integer")
         if value < 0:
             raise ValueError("height must be >= 0")
