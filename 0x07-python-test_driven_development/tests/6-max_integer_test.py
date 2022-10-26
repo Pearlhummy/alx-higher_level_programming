8d288ad5146e0bdc9aa07ef933c1b5370b94474e
Pearlhummy <igbinladepelumi@gmail.com>
2022-10-26 02:55:13 +0000

    task done and updated

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -1,71 +1,43 @@
 #!/usr/bin/python3
 """Unittest for max_integer([..])
 """
+
+
 import unittest
+from subprocess import call
 max_integer = __import__('6-max_integer').max_integer
 
 
 class TestMaxInteger(unittest.TestCase):
-    def test_docstring(self):
-        self.assertTrue(len(__import__('6-max_integer').__doc__) > 1)
+    """Checks for correct output during many edge cases.
+    """
+    def test_simple(self):
+        self.assertEqual(max_integer([1, 2, 3, 4]), 4)
 
-    def test_func_doc(self):
-        self.assertTrue(len(max_integer.__doc__) > 1)
-
-    def test_emptyargs(self):
-        self.assertIsNone(max_integer())
+    def test_negative(self):
+        self.assertEqual(max_integer([-219, -89, -832, -300]), -89)
+        self.assertEqual(max_integer([-219, -89, 832, -300]), 832)
 
     def test_empty(self):
-        self.assertIsNone(max_integer([]), None)
-        self.assertIsNone(max_integer([None]), None)
+        self.assertEqual(max_integer([]), None)
+
+    def test_one_item(self):
+        self.assertEqual(max_integer([42]), 42)
+
+    def test_wrong_tpye(self):
         with self.assertRaises(TypeError):
             max_integer(None)
-        with self.assertRaises(TypeError):
-            max_integer([2, 4, 5, 6, None])
+            max_integer("bell pepper")
+            max_integer([1, 2, 3, "cat"])
+            max_integer([1, 2, (8, 3), 4])
+            max_integer([1, 2, 3.7, 4])
 
-    def test_one(self):
-        self.assertEqual(max_integer([55]), 55)
+    def test_style(self):
+        try:
+            self.assertEqual(call(["pep8", "6-max_integer.py"]), 0)
+        except FileNotFoundError:
+            self.assertTrue(True)
 
-    def test_integers(self):
-        self.assertEqual(max_integer([1, 5, 6, 4]), 6)
-        self.assertEqual(max_integer([55, 55, 55, 55]), 55)
-        self.assertEqual(max_integer([6, 2, 3, 4]), 6)
-        self.assertEqual(max_integer([1, 3, 4, 2]), 4)
-        self.assertEqual(max_integer([1, 1, 1, 1]), 1)
-        self.assertEqual(max_integer([3]), 3)
-
-    def test_integer_neg(self):
-        self.assertEqual(max_integer([-44, -66, -43, -22]), -22)
-        self.assertEqual(max_integer([1, 3, 74, -2]), 74)
-
-    def test_float(self):
-        self.assertEqual(max_integer([2.2, 3.3, 4.4, 5.5]), 5.5)
-        self.assertEqual(max_integer([-44.5, -53.22, -2.224, -3.14]), -2.224)
-
-    def test_string(self):
-        self.assertEqual(max_integer("12345323121"), "5")
-        self.assertEqual(max_integer("1, 2, 3"), "3")
-
-    def test_alpha(self):
-        self.assertEqual(max_integer(["xyz"]), "xyz")
-        self.assertEqual(max_integer(["abc", "xyz"]), "xyz")
-        self.assertEqual(max_integer(["abc", "1", "4", "xyz"]), "xyz")
-
-    def test_other_type(self):
-        self.assertEqual(max_integer([[1, 2, 3, 4], [1, 4, 3, 2]]), [1, 4, 3, 2])
-        self.assertEqual(max_integer((3, 4, 5, 6, 19)), 19)
-        with self.assertRaises(KeyError):
-            max_integer({1: 1, 4: 5, 5: 4})
-
-    def test_mix(self):
-        with self.assertRaises(TypeError):
-            max_integer(["help", 22, "44", 4.5, -3])
-        with self.assertRaises(TypeError):
-            max_integer(["help", [22], "44", 5.5, -55])
-
-    def test_none(self):
-        with self.assertRaises(TypeError):
-            max_integer(None)
 
 if __name__ == "__main__":
     unittest.main()
